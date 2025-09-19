b10aafb74b10a8121b627104873d1ad4d7d499bd
WesleyReis13 <lopeswesley1392@gmail.com>
2025-09-19 20:40:37 -0300

    fix: conexao-com-frontend

--- src/app.js
+++ src/app.js
@@ -2,6 +2,16 @@ const express = require('express');
 const app = express();
 
 
+const cors = require("cors");
+
+app.use(cors());
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
+
+
 app.use('/api/webhook', express.raw({type: 'application/json'}));
 
 
