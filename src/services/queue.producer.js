db2d662ab8da6963d6265380b3fa6413bf2a1454
WesleyReis13 <lopeswesley1392@gmail.com>
2025-10-08 10:03:20 -0300

    feat:relatorio-sucesso-falha

--- src/services/queue.producer.js
+++ src/services/queue.producer.js
@@ -32,8 +32,45 @@ async function addWelcomeMessageJob(to, name, objective, routine, tags) {
   }
 }
 
+const messageBusca = new Queue('whatsapp-messages', { connection: redisConnection });
+
+/**
+ * 
+ * @param {Object} messageData 
+ * @param {string} messageData.name
+ * @param {string} messageData.phone
+ * @param {string} messageData.message
+ * @param {Date} messageData.sendAt
+ * @param {string} messageData.campaignId
+ */
+async function addCampaignMessageJob(messageData) {
+  try {
+    const job = await messageBusca.add(
+      'send-campaign-message',
+      {
+        to: messageData.phone,
+        name: messageData.name,
+        text: messageData.message,
+        sendAt: messageData.sendAt,
+        campaignId: messageData.campaignId
+      },
+      {
+        delay: Math.max(new Date(messageData.sendAt) - Date.now(), 0),
+        attempts: 3,
+      }
+    );
+
+    console.log(`📤 Job de campanha (${job.id}) adicionado para ${messageData.phone}`);
+    return job;
+  } catch (error) {
+    console.error('❌ Erro ao adicionar job de campanha:', error);
+    throw error;
+  }
+}
+
 
 module.exports = { 
-  addWelcomeMessageJob, 
+  addWelcomeMessageJob,
+  addCampaignMessageJob, 
   messageQueue 
 };
